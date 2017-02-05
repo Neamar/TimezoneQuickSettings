79cd98ecd69d673277b287718ac0d191cbd93e23
Neamar <neamar@neamar.fr>
2017-02-05 09:44:27 -0800

    Tile can be added

--- app/src/main/java/fr/neamar/timezonequicksettings/TimezoneTileService.java
+++ app/src/main/java/fr/neamar/timezonequicksettings/TimezoneTileService.java
@@ -1,9 +1,47 @@
 package fr.neamar.timezonequicksettings;
 
+import android.content.SharedPreferences;
 import android.service.quicksettings.TileService;
+import android.util.Log;
 
-/**
- * Created by neamar on 05/02/17.
- */
 public class TimezoneTileService extends TileService {
+    public static final String TAG = "TimezoneTileService";
+
+    public static final String PREFS_NAME = "timezone_tile_service";
+
+    @Override
+    public void onTileAdded() {
+        Log.i(TAG, "Tile added.");
+
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        Log.i(TAG, "Tile removed.");
+
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        Log.i(TAG, "Start listening.");
+        SharedPreferences sp = getBaseContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+        String timezoneToUse = sp.getString("timezone", "");
+
+        if(timezoneToUse.isEmpty()) {
+            Log.i(TAG, "Timezone not defined yet.");
+            getQsTile().setLabel(getString(R.string.tile_label_unitialized));
+        }
+
+        getQsTile().updateTile();
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        Log.i(TAG, "Stop listening.");
+        super.onStopListening();
+    }
 }
