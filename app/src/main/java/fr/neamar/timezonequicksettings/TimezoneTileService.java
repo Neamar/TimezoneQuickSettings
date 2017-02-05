8303db09cd68375d0c94d11456c82e940885f80c
Neamar <neamar@neamar.fr>
2017-02-05 09:32:40 -0800

    Display quicksettings in list of available quicksettings

---
+++ app/src/main/java/fr/neamar/timezonequicksettings/TimezoneTileService.java
@@ -0,0 +1,9 @@
+package fr.neamar.timezonequicksettings;
+
+import android.service.quicksettings.TileService;
+
+/**
+ * Created by neamar on 05/02/17.
+ */
+public class TimezoneTileService extends TileService {
+}
