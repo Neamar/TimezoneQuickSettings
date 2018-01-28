d531e8299e534da003bd89a0bae539c1679c0061
Neamar <neamar@neamar.fr>
2018-01-27 17:22:03 -0800

    Add fastScroll

--- app/src/main/java/fr/neamar/timezonequicksettings/TimezoneAdapter.java
+++ app/src/main/java/fr/neamar/timezonequicksettings/TimezoneAdapter.java
@@ -4,20 +4,27 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.SectionIndexer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by neamar on 27/01/18.
  */
 
-public class TimezoneAdapter extends ArrayAdapter<String> {
+public class TimezoneAdapter extends ArrayAdapter<String> implements SectionIndexer {
     final static String TAG = "TimezoneAdapter";
     final static String NESTED_INDICATOR = " / ...";
 
-    public String prefix;
+    private HashMap<String, Integer> alphaIndexer;
+    private String[] sections;
+
+    String prefix;
 
     // all available timezones
     private final List<String> allOptions;
@@ -62,6 +69,47 @@ public class TimezoneAdapter extends ArrayAdapter<String> {
     }
 
     public String getFullName(int position) {
+        this.notifyDataSetChanged();
         return this.prefix + getItem(position);
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+
+        alphaIndexer = new HashMap<String, Integer>();
+        int size = currentOptions.size();
+
+        for (int x = 0; x < size; x++) {
+            String s = currentOptions.get(x);
+
+            // get the first letter of the store
+            String ch =  s.substring(0, 1);
+            // convert to uppercase otherwise lowercase a -z will be sorted after upper A-Z
+            ch = ch.toUpperCase();
+
+            // HashMap will prevent duplicates
+            alphaIndexer.put(ch, x);
+        }
+
+        Set<String> sectionLetters = alphaIndexer.keySet();
+
+        // create a list from the set to sort
+        ArrayList<String> sectionList = new ArrayList<String>(sectionLetters);
+        Collections.sort(sectionList);
+        sections = new String[sectionList.size()];
+        sectionList.toArray(sections);
+    }
+
+    public int getPositionForSection(int section) {
+        return alphaIndexer.get(sections[section]);
+    }
+
+    public int getSectionForPosition(int position) {
+        return 1;
+    }
+
+    public Object[] getSections() {
+        return sections;
     }
 }
